3211ae8254b35b6f195fa773691bee3a961d2954
georg34523-boop <georg34523@proton.me>
2025-12-04 13:10:41 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Telegram Mini App SDK */}
+        <script src="https://telegram.org/js/telegram-web-app.js" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
